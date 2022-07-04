363df3e8281f829eb80125b1a7b47f28ea3cc9a9
Артем <cooler292@mail.ru>
2022-07-04 20:45:25 +0300

    Добавление ICommand

---
+++ RevitMVVM/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RevitMVVM.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T filed, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(filed, value)) return false;
+            filed = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
